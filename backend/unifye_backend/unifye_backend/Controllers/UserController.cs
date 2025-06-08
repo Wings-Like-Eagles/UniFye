0b5f8cbf8fd53637c498e8fc69e652ae7c993b60
SirknightMike <combrim@gmail.com>
2025-06-08 17:38:33 +0200

    feat(user): added db context to store data in db

---
+++ backend/unifye_backend/unifye_backend/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace unifye_backend.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error() { }
+    }
+}
---
+++ backend/unifye_backend/unifye_backend/Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿namespace unifye_backend.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+    }
+}
